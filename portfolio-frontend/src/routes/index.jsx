e158e3d8c82eacb9364ad745860df42f5d64e2ab
Pappu Kumar <pappukumarme9999@gmail.com>
2025-04-18 09:25:51 +0530

    Deploy to gh-pages

--- portfolio-frontend/src/routes/index.jsx
+++ portfolio-frontend/src/routes/index.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { HomePage, AboutPage, ContactPage, ProjectsPage, ProjectDetailPage, NotFoundPage } from '../pages';
 
 const AppRoutes = () => (
-  <BrowserRouter>
+  <BrowserRouter basename="/portfolio-pappu">
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/about" element={<AboutPage />} />
